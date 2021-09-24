57a2a7d638d1992c5053b8edc10072550ff8b50a
aaron-might <aaronmighty7@gmail.com>
2021-09-24 16:22:27 -0400

    writing functions

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-const pw = document.getElementById('pw');
-const copy =document.getElementById('copy');
-const len = document.getElementById('len');
-const upper= document.getElementById('upper');
-const lower =document.getElementById('lower');//
-const number =document.getElementById('number')//
-const symbol = document.getElementById('symbol');
-const generate = document.getElementById('generate');
+const pwEl = document.getElementById("pw");
+const copyEl =document.getElementById("copy");
+const lenEl = document.getElementById("len");
+const upperEl= document.getElementById("upper");
+const lowerEl =document.getElementById("lower");//
+const numberEl =document.getElementById("number")//
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
 
 
 const upperLetters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerLetters = "abcdefghijklmnopqrstuvyz";
+const lowerLetters ="abcdefghijklmnopqrstuvyz";
 const numbers ="0123456789";
 const symbols ="!@#$%^&*()_+=";
 
 
+function getLowercase (){
+return lowerLetters[Math.floor(Math.random()*lowerLetters.length)];
+}
 
+function getUppercase (){
+return upperLetters[Math.floor(Math.random()*upperLetters.length)];
+}
+
+function getNumber (){
+return numbers[Math.floor(Math.random()*numbers.length)];
+}
+function getSymbols (){
+return symbols[Math.floor(Math.random()*symbols.length)];
+}
+
+alert(getNumber());
